7f04dba7825c0ea5b07fe28ca3343923ab0b32f1
Rodrigo55pereira <rodrigo55pereira.com>
2025-01-23 09:06:35 -0300

    create project TravelApp

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +6,28 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [
+    // Adicione pastas externas ao projeto, se necessário.
+  ],
+  resolver: {
+    // Configurações do resolver, como exclusão de arquivos ou diretórios.
+    blacklistRE: /node_modules\/.*\/node_modules/,
+  },
+  server: {
+    // Limite o número de arquivos que o Metro observa.
+    enhanceMiddleware: (middleware) => {
+      return (req, res, next) => {
+        // Ajuste aqui, se necessário.
+        middleware(req, res, next);
+      };
+    },
+  },
+  transformer: {
+    // Habilite o cache ou minificação, se necessário.
+    minifierPath: 'metro-minify-terser',
+  },
+  maxWorkers: 2, // Reduza o número de threads para reduzir a carga do sistema.
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
