bc7bf4db15352cc12c901067edf7c7bada764a3a
Rodrigo55pereira <rodrigo55pereira.com>
2025-01-30 17:37:59 -0300

    Projeto Gallery

--- src/screens/Gallery/index.tsx
+++ src/screens/Gallery/index.tsx
@@ -1,15 +1,43 @@
 import React from 'react'
 
-import { SafeAreaView, Text } from 'react-native'
+import { FlatList, Image, Pressable, SafeAreaView, TouchableOpacity, View } from 'react-native'
 
 import s from './styles'
+import { useNavigation, useRoute } from '@react-navigation/native'
 
+type GalleryParams = {
+    images: string[]
+}
 
 const Gallery = () => {
 
+    const route = useRoute()
+    const navigation = useNavigation()
+    const { images } = route?.params as GalleryParams || {}
+
+    const onBack = () => {
+        navigation.goBack()
+    }
+
+
     return (
         <SafeAreaView style={s.container}>
-            <Text>Gallery</Text>
+            <View>
+                <FlatList
+                    showsVerticalScrollIndicator={false}
+                    style={{ paddingHorizontal: 24, marginBottom: 32 }}
+                    data={images}
+                    renderItem={({ item }) => (
+                        <Image source={{ uri: item }} style={s.image} />
+                    )}
+                />
+                <Pressable onPress={onBack} hitSlop={8} style={s.backContainer}>
+                    <Image
+                        source={require('../../assets/back.png')}
+                        style={s.backIcon}
+                    />
+                </Pressable>
+            </View>
         </SafeAreaView>
     )
 }
--- src/screens/Gallery/styles.tsx
+++ src/screens/Gallery/styles.tsx
@@ -4,25 +4,19 @@ const s = StyleSheet.create({
     container: {
         flex: 1,
     },
-
-    subtitle: {
-        fontSize: 20,
-        color: '#000000',
-        marginTop: 40,
+    image: {
+        width: '100%',
+        height: 400,
+        borderRadius: 20,
+        marginTop: 24
     },
-    row: {
-        flexDirection: 'row',
-        //backgroundColor: 'red'
-        flexWrap: 'wrap',
+    backContainer: {
+        position: 'absolute',
+        margin:32,
     },
-    emptyView: {
-        flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center',
-    },
-    emptyText: {
-        fontSize: 12,
-        color: 'rgba(0,0,0,0.5)',
+    backIcon: {
+        width: 40,
+        height: 40
     }
 })
 
